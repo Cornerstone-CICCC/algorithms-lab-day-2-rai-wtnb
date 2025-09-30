1ab13c093293ad7615810f9234a4f86eddee2f7f
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-09-29 17:43:17 -0700

    chore: complete ex04.js

--- ex04.js
+++ ex04.js
@@ -2,4 +2,21 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
+function mostFrequentChar(str) {
+  const counter = {};
+  for (let i = 0; i < str.length; i++) {
+    counter[str[i]] = (counter[str[i]] || 0) + 1;
+  }
+
+  let maxCount = 0;
+  let maxChar = "";
+  for (let char in counter) {
+    if (counter[char] > maxCount) {
+      maxCount = counter[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
+
 console.log(mostFrequentChar("javascript")); // Expected output: "a"
