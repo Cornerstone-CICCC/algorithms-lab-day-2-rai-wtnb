1fe145200383bac7f2a82512e67e140f438baa70
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-09-29 17:48:27 -0700

    chore: complete ex06.js

--- ex06.js
+++ ex06.js
@@ -3,4 +3,19 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+function getQueryParams(url) {
+  const dst = {};
+  const params = url.split("?")[1].split("&");
+  for (let i = 0; i < params.length; i++) {
+    const splited = params[i].split("=");
+    if (splited.length !== 2) {
+      console.log("error");
+      continue;
+    }
+    const [key, value] = splited;
+    if (key && value) dst[key] = value;
+  }
+  return dst;
+}
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
