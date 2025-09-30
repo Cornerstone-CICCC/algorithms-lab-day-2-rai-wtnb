5f7ec83d49d6648d39c76cab9dfff2cd69d9b65a
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-09-29 17:49:17 -0700

    chore: complete ex07.js

--- ex07.js
+++ ex07.js
@@ -2,6 +2,10 @@
 // and returns true if the email is valid, and false otherwise. Use basic validation
 // (e.g., the presence of "@" and a ".").
 // Example: validateEmail("test@example.com") should return true.
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
 
 console.log(validateEmail("test@example.com")); // Expected output: true
 console.log(validateEmail("invalid-email.com")); // Expected output: false
